c229258cee1941a35f07a176304ec43ada876b24
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-03 16:29:40 -0300

    docs: add docs to calculate method

--- src/domain/usecases/local-calculate-compound-interest.js
+++ src/domain/usecases/local-calculate-compound-interest.js
@@ -1,6 +1,15 @@
+/** Class responsable to contain methods to calculate Compound Interest */
 export class LocalCalculateCompoundInterest {
   constructor () {}
 
+  /**
+   *
+   * @param {Object} investmentProps - Properties to calculate Compound Interest
+   * @param {number} investmentProps.initialInvestment - The initial investment
+   * @param {number} investmentProps.time - Time in months
+   * @param {number} investmentProps.interestRate - Interest rate
+   * @returns {string}
+   */
   calculate ({ initialInvestment, time, interestRate }) {
     if (!initialInvestment || !time || !interestRate) {
       throw new Error('All fields is required')
--- src/domain/usecases/local-calculate-future-value.js
+++ src/domain/usecases/local-calculate-future-value.js
@@ -1,6 +1,15 @@
+/** Class responsable to contain methods to calculate Future Value */
 export class LocalCalculateFutureValue {
   constructor () {}
 
+  /**
+   *
+   * @param {Object} investmentProps - Properties to calculate Future Value
+   * @param {number} investmentProps.installmentValue - The amount that is added monthly
+   * @param {number} investmentProps.time - Time in months
+   * @param {number} investmentProps.interestRate - Interest rate
+   * @returns {string}
+   */
   calculate ({ installmentValue, time, interestRate }) {
     if (!installmentValue || !time || !interestRate) {
       throw new Error('All fields is required')
