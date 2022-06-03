b3536573e51afd06e70c60f25bf441d6fe6df6c2
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-03 18:53:28 -0300

    test: add TextField component

---
+++ tests/presentation/components/textField.spec.jsx
@@ -0,0 +1,52 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import TextField from '../../../src/presentation/components/TextField'
+
+const makeSut = ({
+  label,
+  labelFor,
+  message,
+  onChange
+}) => {
+  render(
+    <TextField
+      label={label}
+      labelFor={labelFor}
+      message={message}
+      onInput={onChange}
+    />
+  )
+}
+
+describe('<TextField />', () => {
+  it('should render with label', () => {
+    makeSut({ label: 'Investimento inicial', labelFor: 'field' })
+
+    const input = screen.getByLabelText(/investimento inicial/i)
+
+    expect(input).toBeInTheDocument()
+  })
+
+  it('should render without label', () => {
+    makeSut({})
+
+    const input = screen.queryByLabelText(/investimento inicial/i)
+
+    expect(input).not.toBeInTheDocument()
+  })
+
+  it('should changes its value when typing', async () => {
+    const onChange = jest.fn()
+    makeSut({ label: 'Investimento inicial', labelFor: 'field', onChange })
+
+    const input = screen.getByLabelText(/investimento inicial/i)
+    const text = 'This is my new text!'
+    await userEvent.type(input, text)
+
+    await waitFor(() => input.nodeValue)
+
+    expect(input).toHaveValue(text)
+    expect(onChange).toHaveBeenCalledTimes(text.length)
+  })
+})
