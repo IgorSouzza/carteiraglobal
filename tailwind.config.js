7f4784af12e7e77c8ae3387eb61636e02e0cca09
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-03 16:44:54 -0300

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,jsx}',
+    './src/presentation/templates/**/*.{js,jsx}',
+    './src/presentation/components/**/*.{js,jsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
