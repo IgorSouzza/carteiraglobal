3dc8c8d04b7a804cc7f93aa987f0fe5b2dbee049
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-04 14:16:06 -0300

    refactor: move styles to its own layer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 
-import '../styles/globals.css'
+import '../presentation/styles/globals.css'
 
 function MyApp ({ Component, pageProps }) {
   return (
