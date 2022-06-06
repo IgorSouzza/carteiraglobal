294014303e08964e246c25789fa2568e37d2f645
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-06 16:59:17 -0300

    test: add FinancialProjection component

---
+++ tests/presentation/components/financialProjection.spec.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import FinancialProjection from '../../../src/presentation/components/FinancialProjection'
+
+const makeSut = () => {
+  render(<FinancialProjection />)
+}
+
+describe('<FinancialProjection />', () => {
+  it('should render chart correctly', () => {
+    makeSut()
+
+    const chart = screen.getByTestId('chart-container')
+
+    expect(chart).toBeInTheDocument()
+    expect(chart.firstChild).toHaveAttribute('data-highcharts-chart')
+  })
+})
