5d8449ac295ea2dd857c34f6c596d5d714e2bd67
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-07 18:01:29 -0300

    docs: add JSDOC to util formatter

--- src/presentation/utils/format.js
+++ src/presentation/utils/format.js
@@ -1,3 +1,8 @@
+/**
+ * Format a number to percent string
+ * @param {number} value - number to be formated
+ * @returns
+ */
 export function formatToPercent (value) {
   return value.toLocaleString('pt-br', {
     style: 'percent',
@@ -6,6 +11,11 @@ export function formatToPercent (value) {
   })
 }
 
+/**
+ * Format a number to currency string
+ * @param {number | bigint} value - number to be formated
+ * @returns
+ */
 export function formatToCurrency (value) {
   const formatter = new Intl.NumberFormat('pt-br', {
     style: 'currency',
