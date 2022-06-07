6d6a1d3218d96cc7f6488d762e4d27d3d155da38
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-07 18:07:03 -0300

    docs: add JSDOC to RemoteGetAccumulatedValue

--- src/data/usecases/remote-get-accumulated-value.js
+++ src/data/usecases/remote-get-accumulated-value.js
@@ -1,12 +1,24 @@
+/** Class responsable to get and return information from HttpClient */
 export class RemoteGetAccumulatedValue {
   url = ''
   httpClient = null
 
+  /**
+   * Variables needed to call adapter request
+   * @param {string} url - URL will be sent in load function
+   * @param {ApolloAdapter} httpClient - Adapter to make a call
+   */
   constructor (url, httpClient) {
     this.url = url
     this.httpClient = httpClient
   }
 
+  /**
+   * Make a request to some URL
+   * @param {Object} data - Data to be sent to the API
+   * @param {string} data.query - GraphQL Query
+   * @returns {any}
+   */
   async load (data) {
     const httpResponse = await this.httpClient.request({
       url: this.url,
