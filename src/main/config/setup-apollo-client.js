7399441dee211637fa145a75718508e7cd788b3b
Igor Souza <igor.souza.webmaster@gmail.com>
2022-06-08 15:43:31 -0300

    chore: remove addTypename from ApolloClient

--- src/main/config/setup-apollo-client.js
+++ src/main/config/setup-apollo-client.js
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const apolloClient = new ApolloClient({
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({
+    addTypename: false
+  })
 })
